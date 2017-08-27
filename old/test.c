fd5356f41dbd319ba3f197722ba628bb79f58cd4
kepade14 <kepade12@gmail.com>
2017-08-27 20:03:53 +0800

    change all

---
+++ old/test.c
@@ -0,0 +1,12 @@
+//this is just for a test on github
+#include<stdio.h>
+
+int main(void)
+{
+	int test=0;
+	printf("please input a word to test");
+
+	scanf("%d",&test);
+	printf("successful!");
+	return 0;
+}
